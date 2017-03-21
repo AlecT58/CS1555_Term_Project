10bb67402bd5eab876498bb6180c132878c7b58e
atrievel <atrievel@gmail.com>
2017-03-21 15:04:27 -0400

    Merged Schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,3 @@
---MASTER SCHEMA--
-
-
 --Drop Tables for consistency--
 drop table MUTUALFUND cascade constraints;
 drop table CLOSINGPRICE cascade constraints;
@@ -12,44 +9,59 @@ drop table TRXLOG cascade constraints;
 drop table OWNS cascade constraints;
 drop table MUTUALDATE cascade constraints;
 
+--First 5 tables from part 1
 
+CREATE TABLE MUTUALFUND
+(
+    symbol VARCHAR2(20) PRIMARY KEY,
+    name VARCHAR2(30),
+    description VARCHAR2(100),
+    category VARCHAR2(10),
+    c_date DATE
+);
 
+CREATE TABLE CLOSINGPRICE
+(
+    symbol VARCHAR2(20),
+    price FLOAT,
+    p_date DATE,
+    CONSTRAINT closing_pk 
+        PRIMARY KEY (symbol, p_date),
+    CONSTRAINT closing_fk
+        FOREIGN KEY (symbol) 
+        REFERENCES MUTUALFUND (symbol)
+);
 
+CREATE TABLE CUSTOMER
+(
+    login VARCHAR2(10) PRIMARY KEY,
+    name VARCHAR2(20),
+    email VARCHAR2(30),
+    address VARCHAR2(30),
+    password VARCHAR2(10),
+    balance FLOAT
+);
 
+CREATE TABLE ADMINISTRATOR
+(
+    login VARCHAR2(10) PRIMARY KEY,
+    name VARCHAR2(20),
+    email VARCHAR2(30),
+    address VARCHAR2(30),
+    password VARCHAR2(10),
+);
 
+CREATE TABLE ALLOCATION
+(
+    allocation_no INT PRIMARY KEY,
+    symbol VARCHAR2(20),
+    percentage FLOAT,
+    CONSTRAINT allocation_fk
+        FOREIGN KEY (login)
+        REFERENCES CUSTOMER (login)
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+--Last 4 tables
 create table PREFERS (
 allocation_no integer;
 symbol varchar2(20),
@@ -91,29 +103,4 @@ c_date date,
 constraint pk_mutualdate primary key (c_date)
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+COMMIT;
