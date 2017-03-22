6dbbac9afcde21b68c76c500cffa75e42d4af925
Alec Trievel <atrievel@gmail.com>
2017-03-22 14:28:58 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,8 +9,8 @@ drop table TRXLOG cascade constraints;
 drop table OWNS cascade constraints;
 drop table MUTUALDATE cascade constraints;
 
+--START CREATE TABLES
 --First 5 tables from part 1
-
 CREATE TABLE MUTUALFUND
 (
     symbol VARCHAR2(20),
@@ -116,11 +116,71 @@ create table MUTUALDATE (
 c_date date,
 constraint pk_mutualdate primary key (c_date)
 );
-
+--END CREATE TABLES
 COMMIT;
 
---ADD INSERT STAEMENTS HERE
+--START INSERT STAEMEMENTS
+--Inserts for MUTUALDATE
+INSERT INTO MUTUALDATE (c_date)
+VALUES ('04-APR-14');
 
+--Inserts into CUSTOMER
+INSERT INTO CUSTOMER (login, name, email, address, password, balance)
+VALUES ('mike', 'Mike', 'mike@betterfuture.com', '1st street', 'pwd', 750);
+
+INSERT INTO CUSTOMER (login, name, email, address, password, balance)
+VALUES ('mary', 'Mary', 'mary@betterfuture.com', '2st street', 'pwd', 0);
+
+--Inserts into ADMINISTRATOR
+INSERT INTO ADMINISTRATOR (login, name, email, address, password)
+VALUES ('admin', 'Administrator', 'admin@betterfuture.com', '5th Ave, Pitt', 'root');
+
+--Inserts into MUTUALFUND
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('MM', 'money-market', 'money market, conservative', 'fixed', '06-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('RE', 'real-estate', 'real estate', 'fixed', '09-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('STB', 'short-time-bonds', 'short term bonds', 'bonds', '10-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('LTB', 'long-time-bonds', 'long term bonds', 'bonds', '11-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('BBS', 'balance-time-bonds', 'balance bonds and stocks', 'mixed', '12-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('SRBS', 'social-respons-bonds-stocks', 'social responsibility bonds and stocks', 'mixed', '12-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('GS', 'general-stocks', 'general stocks', 'stocks', '16-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('AS', 'aggressive-stocks', 'aggressive stocks', 'stocks', '23-JAN-14');
+
+INSERT INTO MUTUALFUND (symbol, name, description, category, c_date)
+VALUES ('IMS', 'international-markets-stock', 'international markets stock, risky', 'stocks', '30-JAN-14');
+
+--Inserts into OWNS
+INSERT INTO OWNS (login, symbol, shares)
+VALUES ('mike', 'RE', 50);
+
+--Inserts into TRXLOG
+INSERT INTO TRXLOG (trans_id, login, t_date, action, num_shares, price, amount)
+VALUES (0, 'mike', NULL, '29-MAR-14', 'deposit', NULL, NULL, 1000);
+
+INSERT INTO TRXLOG (trans_id, login, t_date, action, num_shares, price, amount)
+VALUES (1, 'mike', 'MM', '29-MAR-14', 'buy', 50, 10, 500);
+
+INSERT INTO TRXLOG (trans_id, login, t_date, action, num_shares, price, amount)
+VALUES (2, 'mike', 'RE', '29-MAR-14', 'buy', 50, 10, 500);
+
+INSERT INTO TRXLOG (trans_id, login, t_date, action, num_shares, price, amount)
+VALUES (3, 'mike', 'RE', '01-APR-14', 'sell', 50, 15, 750);
+
+--Inserts into 
 COMMIT;
 
 --ADD TRIGGERS/VIEWS HERE
